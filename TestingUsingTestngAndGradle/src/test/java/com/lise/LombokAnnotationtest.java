8da9c62795aede04eb860607f56211becf5225d1
jitendra <jitendra-tawar>
2021-04-02 23:12:14 +0530

    TestWithGradle

---
+++ TestingUsingTestngAndGradle/src/test/java/com/lise/LombokAnnotationtest.java
@@ -0,0 +1,13 @@
+package com.lise;
+import lombok.*;
+
+//@Data
+@NoArgsConstructor @Getter @Setter @AllArgsConstructor @ToString
+public class LombokAnnotationtest {
+    //@Getter @Setter
+    private String baseURL = "https://reqres.in/api/";
+    //    @Getter @Setter
+    private String email ="eve.holt@reqres.in";
+    //    @Getter @Setter
+    private String password = "piston";
+}
